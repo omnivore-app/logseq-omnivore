f294da28c7f8ab5a41970256ae07aa1ee09c9558
Hongbo Wu <hongbo@omnivore.app>
2022-06-07 22:33:33 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,27 @@
+## Omnivore
+
+This is a sample that show you how to manipulate blocks :)
+
+### API
+
+[![npm version](https://badge.fury.io/js/%40logseq%2Flibs.svg)](https://badge.fury.io/js/%40logseq%2Flibs)
+
+##### [Logseq.Editor](https://logseq.github.io/plugins/interfaces/ieditorproxy.html)
+
+- `insertBlock: (
+  srcBlock: BlockIdentity, content: string, opts?: Partial<{ before: boolean; sibling: boolean; properties: {} }>
+  ) => Promise<BlockEntity | null>`
+- `insertBatchBlock: (
+  srcBlock: BlockIdentity, batch: IBatchBlock | Array<IBatchBlock>, opts?: Partial<{ before: boolean, sibling: boolean }>
+  ) => Promise<Array<BlockEntity> | null>`
+- `updateBlock: (
+  srcBlock: BlockIdentity, content: string, opts?: Partial<{ properties: {} }>
+  ) => Promise<void>`
+- `removeBlock: (
+  srcBlock: BlockIdentity
+  )`
+
+### Running the Sample
+
+- `npm install && npm run build` in terminal to install dependencies.
+- `Load unpacked plugin` in Logseq Desktop client.
---
+++ index.ts
@@ -0,0 +1,107 @@
+import "@logseq/libs";
+import { LSPluginBaseInfo } from "@logseq/libs/dist/libs";
+
+const delay = (t = 100) => new Promise((r) => setTimeout(r, t));
+
+async function loadOmnivoreData(token: string) {
+  const {
+    data: {
+      search: { edges },
+    },
+  } = await fetch("http://localhost:4000/api/graphql", {
+    headers: {
+      "content-type": "application/json",
+      authorization: token,
+    },
+    body: '{"query":"\\n    query Search($after: String, $first: Int, $query: String) {\\n      search(first: $first, after: $after, query: $query) {\\n        ... on SearchSuccess {\\n          edges {\\n            cursor\\n            node {\\n              id\\n              title\\n              slug\\n              url\\n              pageType\\n              contentReader\\n              createdAt\\n              isArchived\\n              readingProgressPercent\\n              readingProgressAnchorIndex\\n              author\\n              image\\n              description\\n              publishedAt\\n              ownedByViewer\\n              originalArticleUrl\\n              uploadFileId\\n              labels {\\n                id\\n                name\\n                color\\n              }\\n              pageId\\n              shortId\\n              quote\\n              annotation\\n              state\\n              siteName\\n            }\\n          }\\n          pageInfo {\\n            hasNextPage\\n            hasPreviousPage\\n            startCursor\\n            endCursor\\n            totalCount\\n          }\\n        }\\n        ... on SearchError {\\n          errorCodes\\n        }\\n      }\\n    }\\n  ","variables":{"after":"0","first":1}}',
+    method: "POST",
+  }).then((res) => res.json());
+
+  const ret = edges || [];
+
+  return ret.map(({ node }, i) => {
+    const { title, url, author, description, slug } = node;
+
+    return `${i}. [${title}](${url}) [:small.opacity-50 "🔥 ${author} 💬 ${slug}"]
+collapsed:: true    
+> ${description}`;
+  });
+}
+
+/**
+ * main entry
+ * @param baseInfo
+ */
+function main(baseInfo: LSPluginBaseInfo) {
+  let loading = false;
+  const { token } = logseq.settings;
+
+  logseq.provideModel({
+    async loadOmnivore() {
+      const info = await logseq.App.getUserConfigs();
+      if (loading) return;
+
+      const pageName = "Omnivore";
+      const blockTitle = new Date().toLocaleString();
+
+      logseq.App.pushState("page", { name: pageName });
+
+      await delay(300);
+
+      loading = true;
+
+      try {
+        const currentPage = await logseq.Editor.getCurrentPage();
+        if (currentPage?.originalName !== pageName)
+          throw new Error("page error");
+
+        const pageBlocksTree = await logseq.Editor.getCurrentPageBlocksTree();
+        let targetBlock = pageBlocksTree[0]!;
+
+        targetBlock = await logseq.Editor.insertBlock(
+          targetBlock.uuid,
+          "🚀 Fetching Omnivore ...",
+          { before: true }
+        );
+
+        let blocks = await loadOmnivoreData(token);
+
+        blocks = blocks.map((it) => ({ content: it }));
+
+        await logseq.Editor.insertBatchBlock(targetBlock.uuid, blocks, {
+          sibling: false,
+        });
+
+        await logseq.Editor.updateBlock(
+          targetBlock.uuid,
+          `## 🔖 Omnivore - ${blockTitle}`
+        );
+      } catch (e) {
+        logseq.App.showMsg(e.toString(), "warning");
+        console.error(e);
+      } finally {
+        loading = false;
+      }
+    },
+  });
+
+  logseq.App.registerUIItem("toolbar", {
+    key: "logseq-omnivore",
+    template: `
+      <a data-on-click="loadOmnivore"
+         class="button">
+        <i class="ti ti-brand-reddit"></i>
+      </a>
+    `,
+  });
+
+  logseq.provideStyle(`
+    [data-injected-ui=logseq-reddit-${baseInfo.id}] {
+      display: flex;
+      align-items: center;
+    }
+  `);
+}
+
+// bootstrap
+logseq.ready(main).catch(console.error);
