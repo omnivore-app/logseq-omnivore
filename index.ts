f9c734343c423aa3b94e3098c279443590dfcfc3
Hongbo Wu <hongbo@omnivore.app>
2022-06-19 10:54:42 +0800

    Lint code

--- index.ts
+++ index.ts
@@ -1,29 +1,63 @@
 import '@logseq/libs'
 import { LSPluginBaseInfo } from '@logseq/libs/dist/LSPlugin'
-import icon from './icon.png'
+import * as icon from './icon.png'
 
 const delay = (t = 100) => new Promise((r) => setTimeout(r, t))
 const endpoint = 'https://api-prod.omnivore.app/api/graphql'
+
+interface GetArticleResponse {
+  data: {
+    article: {
+      article: Article
+    }
+  }
+}
+
+interface SearchResponse {
+  data: {
+    search: {
+      edges: { node: Article }[]
+      pageInfo: {
+        hasNextPage: boolean
+      }
+    }
+  }
+}
+
+interface Article {
+  title: string
+  author: string
+  description: string
+  slug: string
+  labels: Label[]
+  highlights: Highlight[]
+  savedAt: string
+}
+
+interface Label {
+  name: string
+}
+
+interface Highlight {
+  quote: string
+}
 
 async function loadArticle(
   username: string,
   slug: string,
   token: string
-): Promise<any> {
-  const {
-    data: {
-      article: { article },
-    },
-  } = await fetch(endpoint, {
+): Promise<Article> {
+  const res = await fetch(endpoint, {
     headers: {
       'content-type': 'application/json',
       authorization: token,
     },
-    body: `{\"query\":\"\\n  query GetArticle(\\n    $username: String!\\n    $slug: String!\\n    $includeFriendsHighlights: Boolean\\n  ) {\\n    article(username: $username, slug: $slug) {\\n      ... on ArticleSuccess {\\n        article {\\n          ...ArticleFields\\n          content\\n          highlights(input: { includeFriends: $includeFriendsHighlights }) {\\n            ...HighlightFields\\n          }\\n          labels {\\n            ...LabelFields\\n          }\\n        }\\n      }\\n      ... on ArticleError {\\n        errorCodes\\n      }\\n    }\\n  }\\n  \\n  fragment ArticleFields on Article {\\n    id\\n    title\\n    url\\n    author\\n    image\\n    savedAt\\n    createdAt\\n    publishedAt\\n    contentReader\\n    originalArticleUrl\\n    readingProgressPercent\\n    readingProgressAnchorIndex\\n    slug\\n    isArchived\\n    description\\n    linkId\\n    state\\n  }\\n\\n  \\n  fragment HighlightFields on Highlight {\\n    id\\n    shortId\\n    quote\\n    prefix\\n    suffix\\n    patch\\n    annotation\\n    createdByMe\\n    updatedAt\\n    sharedAt\\n  }\\n\\n  \\n  fragment LabelFields on Label {\\n    id\\n    name\\n    color\\n    description\\n    createdAt\\n  }\\n\\n\",\"variables\":{\"username\":\"${username}\",\"slug\":\"${slug}\",\"includeFriendsHighlights\":false}}`,
+    body: `{"query":"\\n  query GetArticle(\\n    $username: String!\\n    $slug: String!\\n    $includeFriendsHighlights: Boolean\\n  ) {\\n    article(username: $username, slug: $slug) {\\n      ... on ArticleSuccess {\\n        article {\\n          ...ArticleFields\\n          content\\n          highlights(input: { includeFriends: $includeFriendsHighlights }) {\\n            ...HighlightFields\\n          }\\n          labels {\\n            ...LabelFields\\n          }\\n        }\\n      }\\n      ... on ArticleError {\\n        errorCodes\\n      }\\n    }\\n  }\\n  \\n  fragment ArticleFields on Article {\\n    id\\n    title\\n    url\\n    author\\n    image\\n    savedAt\\n    createdAt\\n    publishedAt\\n    contentReader\\n    originalArticleUrl\\n    readingProgressPercent\\n    readingProgressAnchorIndex\\n    slug\\n    isArchived\\n    description\\n    linkId\\n    state\\n  }\\n\\n  \\n  fragment HighlightFields on Highlight {\\n    id\\n    shortId\\n    quote\\n    prefix\\n    suffix\\n    patch\\n    annotation\\n    createdByMe\\n    updatedAt\\n    sharedAt\\n  }\\n\\n  \\n  fragment LabelFields on Label {\\n    id\\n    name\\n    color\\n    description\\n    createdAt\\n  }\\n\\n","variables":{"username":"${username}","slug":"${slug}","includeFriendsHighlights":false}}`,
     method: 'POST',
-  }).then((res) => res.json())
+  })
+  const response = (await res.json()) as GetArticleResponse
 
-  return article
+  return response.data.article.article
 }
 
 async function loadArticles(
@@ -31,12 +65,8 @@ async function loadArticles(
   after = 0,
   first = 10,
   savedAfter = ''
-): Promise<[any[], boolean]> {
-  const {
-    data: {
-      search: { edges, pageInfo },
-    },
-  } = await fetch(endpoint, {
+): Promise<[Article[], boolean]> {
+  const res = await fetch(endpoint, {
     headers: {
       'content-type': 'application/json',
       authorization: token,
@@ -45,11 +75,13 @@ async function loadArticles(
       savedAfter ? 'saved:' + savedAfter : ''
     } sort:saved-asc"}}`,
     method: 'POST',
-  }).then((res) => res.json())
+  })
 
-  const articles = edges.map((e: { node: any }) => e.node)
+  const jsonRes = (await res.json()) as SearchResponse
 
-  return [articles, pageInfo.hasNextPage]
+  const articles = jsonRes.data.search.edges.map((e) => e.node)
+
+  return [articles, jsonRes.data.search.pageInfo.hasNextPage]
 }
 
 /**
@@ -58,8 +90,8 @@ async function loadArticles(
  */
 function main(baseInfo: LSPluginBaseInfo) {
   let loading = false
-  const token = logseq.settings?.['api key']
-  const username = logseq.settings?.['username']
+  const token = logseq.settings?.['api key'] as string
+  const username = logseq.settings?.['username'] as string
 
   logseq.provideModel({
     async loadOmnivore() {
@@ -80,7 +112,7 @@ function main(baseInfo: LSPluginBaseInfo) {
       loading = true
 
       try {
-        logseq.UI.showMsg('🚀 Fetching articles ...')
+        await logseq.UI.showMsg('🚀 Fetching articles ...')
 
         const currentPage = await logseq.Editor.getCurrentPage()
         if (currentPage?.originalName !== pageName)
@@ -91,7 +123,7 @@ function main(baseInfo: LSPluginBaseInfo) {
         let lastUpdateAt = ''
         if (targetBlock) {
           const matches = targetBlock.content.match(
-            /(\d{4})-(\d{2})-(\d{2})T(\d{2})\:(\d{2})\:(\d{2}).(\d{3})Z/g
+            /(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}).(\d{3})Z/g
           )
           if (matches) {
             lastUpdateAt = matches[0]
@@ -105,6 +137,7 @@ function main(baseInfo: LSPluginBaseInfo) {
 
         const size = 100
         let after = 0
+        /*eslint no-constant-condition: ["error", { "checkLoops": false }]*/
         while (true) {
           const [articles, hasNextPage] = await loadArticles(
             token,
@@ -180,7 +213,7 @@ function main(baseInfo: LSPluginBaseInfo) {
     key: 'logseq-omnivore',
     template: `
       <a data-on-click="loadOmnivore" class="button" style="width:2rem;height:2rem;">
-        <img src="${icon}">
+        <img src="${icon as string}">
       </a>
     `,
   })
