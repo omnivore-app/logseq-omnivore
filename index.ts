6b5d6bfa3cd443e705b57def78355e1c89ba9231
Hongbo Wu <hongbo@omnivore.app>
2022-06-22 12:13:52 +0800

    Fetch articles on startup and sync every minute

--- index.ts
+++ index.ts
@@ -1,5 +1,9 @@
 import '@logseq/libs'
-import { LSPluginBaseInfo, SettingSchemaDesc } from '@logseq/libs/dist/LSPlugin'
+import {
+  BlockEntity,
+  LSPluginBaseInfo,
+  SettingSchemaDesc,
+} from '@logseq/libs/dist/LSPlugin'
 import * as icon from './icon.png'
 
 interface GetArticleResponse {
@@ -55,10 +59,9 @@ const settings: SettingSchemaDesc[] = [
     default: '',
   },
 ]
-logseq.useSettingsSchema(settings)
-
 const endpoint = 'https://api-prod.omnivore.app/api/graphql'
 const delay = (t = 100) => new Promise((r) => setTimeout(r, t))
+let loading = false
 
 const loadArticle = async (
   username: string,
@@ -101,12 +104,136 @@ const loadArticles = async (
   return [articles, jsonRes.data.search.pageInfo.hasNextPage]
 }
 
+const loadOmnivore = async (
+  apiKey: string,
+  username: string
+): Promise<void> => {
+  if (loading) return
+
+  const pageName = 'Omnivore'
+  const blockTitle = '## 🔖 Articles'
+  const highlightTitle = '### 🔍 [[Highlights]]'
+  const fetchingTitle = '🚀 Fetching articles ...'
+
+  logseq.App.pushState('page', { name: pageName })
+
+  await delay(300)
+
+  loading = true
+  let targetBlock: BlockEntity | null = null
+
+  try {
+    await logseq.UI.showMsg('🚀 Fetching articles ...')
+
+    const currentPage = await logseq.Editor.getCurrentPage()
+    if (currentPage?.originalName !== pageName) throw new Error('page error')
+
+    const pageBlocksTree = await logseq.Editor.getCurrentPageBlocksTree()
+    targetBlock = pageBlocksTree.length > 0 ? pageBlocksTree[0] : null
+    let lastUpdateAt = ''
+    if (targetBlock) {
+      const matches = targetBlock.content.match(
+        /(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}).(\d{3})Z/g
+      )
+      if (matches) {
+        lastUpdateAt = matches[0]
+      }
+      await logseq.Editor.updateBlock(targetBlock.uuid, fetchingTitle)
+    } else {
+      targetBlock = await logseq.Editor.insertBlock('', fetchingTitle, {
+        before: true,
+      })
+    }
+    if (!targetBlock) {
+      throw new Error('block error')
+    }
+
+    const size = 100
+    let after = 0
+    /*eslint no-constant-condition: ["error", { "checkLoops": false }]*/
+    while (true) {
+      const [articles, hasNextPage] = await loadArticles(
+        apiKey,
+        after,
+        size,
+        lastUpdateAt
+      )
+
+      for (const { title, author, slug, description } of articles) {
+        const { labels, highlights, savedAt } = await loadArticle(
+          username,
+          slug,
+          apiKey
+        )
+
+        const content = `[${title}](https://omnivore.app/${username}/${slug})
+            author:: ${author}
+            description:: ${description}
+            labels:: ${
+              labels
+                ? labels.map((l: { name: string }) => `[[${l.name}]]`).join(' ')
+                : 'null'
+            }
+            date:: ${new Date(savedAt).toDateString()}`
+
+        const articleBlock = await logseq.Editor.insertBlock(
+          targetBlock.uuid,
+          content,
+          { before: true, sibling: false }
+        )
+        if (!articleBlock) {
+          throw new Error('block error')
+        }
+
+        if (highlights?.length) {
+          const highlightBlock = await logseq.Editor.insertBlock(
+            articleBlock.uuid,
+            highlightTitle
+          )
+          if (!highlightBlock) {
+            throw new Error('block error')
+          }
+
+          for (const highlight of highlights) {
+            await logseq.Editor.insertBlock(
+              highlightBlock.uuid,
+              highlight.quote
+            )
+          }
+        }
+
+        // sleep for a second to avoid rate limit
+        await delay(1000)
+      }
+
+      if (!hasNextPage) {
+        break
+      }
+      after += size
+    }
+
+    await logseq.UI.showMsg('🔖 Articles fetched')
+  } catch (e) {
+    await logseq.UI.showMsg('Failed to fetch articles', 'warning')
+    console.error(e)
+  } finally {
+    loading = false
+    targetBlock &&
+      (await logseq.Editor.updateBlock(
+        targetBlock.uuid,
+        `${blockTitle} [:small.opacity-20 "fetched at ${new Date().toISOString()}"]`
+      ))
+  }
+}
+
 /**
  * main entry
  * @param baseInfo
  */
 const main = async (baseInfo: LSPluginBaseInfo): Promise<void> => {
   console.log('logseq-omnivore loaded')
+
+  logseq.useSettingsSchema(settings)
 
   let apiKey = logseq.settings?.['api key'] as string
   let username = logseq.settings?.['username'] as string
@@ -122,125 +249,9 @@ const main = async (baseInfo: LSPluginBaseInfo): Promise<void> => {
     username = logseq.settings?.['username'] as string
   })
 
-  let loading = false
-
   logseq.provideModel({
     async loadOmnivore() {
-      if (loading) return
-
-      const pageName = 'Omnivore'
-      const blockTitle = '## 🔖 Articles'
-      const highlightTitle = '### 🔍 [[Highlights]]'
-      const fetchingTitle = '🚀 Fetching articles ...'
-
-      logseq.App.pushState('page', { name: pageName })
-
-      await delay(300)
-
-      loading = true
-
-      try {
-        await logseq.UI.showMsg('🚀 Fetching articles ...')
-
-        const currentPage = await logseq.Editor.getCurrentPage()
-        if (currentPage?.originalName !== pageName)
-          throw new Error('page error')
-
-        const pageBlocksTree = await logseq.Editor.getCurrentPageBlocksTree()
-        let targetBlock = pageBlocksTree.length > 0 ? pageBlocksTree[0] : null
-        let lastUpdateAt = ''
-        if (targetBlock) {
-          const matches = targetBlock.content.match(
-            /(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}).(\d{3})Z/g
-          )
-          if (matches) {
-            lastUpdateAt = matches[0]
-          }
-          await logseq.Editor.updateBlock(targetBlock.uuid, fetchingTitle)
-        } else {
-          targetBlock = await logseq.Editor.insertBlock('', fetchingTitle, {
-            before: true,
-          })
-        }
-        if (!targetBlock) {
-          throw new Error('block error')
-        }
-
-        const size = 100
-        let after = 0
-        /*eslint no-constant-condition: ["error", { "checkLoops": false }]*/
-        while (true) {
-          const [articles, hasNextPage] = await loadArticles(
-            apiKey,
-            after,
-            size,
-            lastUpdateAt
-          )
-
-          for (const { title, author, slug, description } of articles) {
-            const { labels, highlights, savedAt } = await loadArticle(
-              username,
-              slug,
-              apiKey
-            )
-
-            const content = `[${title}](https://omnivore.app/${username}/${slug})
-            author:: ${author}
-            description:: ${description}
-            labels:: ${
-              labels
-                ? labels.map((l: { name: string }) => `[[${l.name}]]`).join(' ')
-                : 'null'
-            }
-            date:: ${new Date(savedAt).toDateString()}`
-
-            const articleBlock = await logseq.Editor.insertBlock(
-              targetBlock.uuid,
-              content,
-              { before: true, sibling: false }
-            )
-            if (!articleBlock) {
-              throw new Error('block error')
-            }
-
-            if (highlights?.length) {
-              const highlightBlock = await logseq.Editor.insertBlock(
-                articleBlock.uuid,
-                highlightTitle
-              )
-              if (!highlightBlock) {
-                throw new Error('block error')
-              }
-
-              for (const highlight of highlights) {
-                await logseq.Editor.insertBlock(
-                  highlightBlock.uuid,
-                  highlight.quote
-                )
-              }
-            }
-
-            // sleep for a second to avoid rate limit
-            await delay(1000)
-          }
-
-          if (!hasNextPage) {
-            break
-          }
-          after += size
-        }
-
-        await logseq.Editor.updateBlock(
-          targetBlock.uuid,
-          `${blockTitle} [:small.opacity-20 "fetched at ${new Date().toISOString()}"]`
-        )
-        await logseq.UI.showMsg('🔖 Articles fetched')
-      } catch (e) {
-        await logseq.UI.showMsg('Failed to fetch articles', 'warning')
-        console.error(e)
-      } finally {
-        loading = false
-      }
+      await loadOmnivore(apiKey, username)
     },
   })
 
@@ -259,6 +270,14 @@ const main = async (baseInfo: LSPluginBaseInfo): Promise<void> => {
       align-items: center;
     }
   `)
+
+  // fetch articles on startup
+  await loadOmnivore(apiKey, username)
+
+  // fetch articles every minute
+  setInterval(async () => {
+    await loadOmnivore(apiKey, username)
+  }, 60000)
 }
 
 // bootstrap
