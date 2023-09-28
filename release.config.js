cd61130002343471528b3d52e3c41b060bd6924a
Hongbo Wu <hongbo@omnivore.app>
2023-09-28 21:33:44 +0800

    auto-release on release branch

--- release.config.js
+++ release.config.js
@@ -1,7 +1,7 @@
 const pluginName = require('./package.json').name
 
 module.exports = {
-  branches: 'main',
+  branches: 'release',
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
